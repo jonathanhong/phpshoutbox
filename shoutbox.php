a048bec94bda7193ee5f26ca7d06593be1e907b3
Jonathan Hong <jhong@cecs.pdx.edu>
2014-08-28 20:19:43 -0700

    added shoutbox user

--- shoutbox.php
+++ shoutbox.php
@@ -1,5 +1,6 @@
 <?php
   require_once("connection.php");
+  $curr_user = "Myself";
 ?>
 <div id="shoutbox">
 <?php	
@@ -10,14 +11,13 @@
 		<div class="shout">
 			<div class="suser">[<?= date("M d g:ia", $row['date_time']) ?>]<?= $row['name'] ?></div>
 			<div class="smessage"><strong><?= $row['message'] ?></strong></div>
-		</div><?= $messages ?>
+		</div>
 <?php
   }
 ?>	
-<?= $messages ?>
 </div>
 <form method="post" action="shout.php">
-	<input type="hidden" id="sbname" name="name" value="<?= $row['name'] ?>">
+	<input type="hidden" id="sbname" name="name" value="<?= $curr_user ?>">
 	Message: <textarea type="text" id="sbmessage" name="message" class="message"></textarea>
 	<input type="submit" id="sbsubmit" onclick="return sb_message();" value="Submit" />
 	<input type="submit" id="sbupdate" onclick="return sb_refresh();" value="Refresh" />	
